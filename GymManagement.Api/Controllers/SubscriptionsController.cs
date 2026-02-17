3923a0af3286af01d18cd07a9ce97795235eba23
Wayfaring Tenno <120097440+Tenno641@users.noreply.github.com>
2026-02-17 16:31:15 +0200

    Feature/subscriptions (#1)
    * [ADD] Subscriptions - Presentation Layer
    
    * [ADD] Subscriptions - Application Layer

---
+++ GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,27 @@
+﻿using GymManagement.Application.Services;
+using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    private readonly ISubscriptionsService _subscriptionsService;
+    
+    public SubscriptionsController(ISubscriptionsService subscriptionsService)
+    {
+        _subscriptionsService = subscriptionsService;
+    }
+    
+    [HttpPost]
+    public IActionResult Create(CreateSubscription request)
+    {
+        Guid newSubscriptionId = _subscriptionsService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
+
+        SubscriptionResponse response = new SubscriptionResponse(newSubscriptionId, request.SubscriptionType);
+
+        return Ok(response);
+    }
+}
--- GymManagement.Api/Program.cs
+++ GymManagement.Api/Program.cs
@@ -1,7 +1,22 @@
+using System.Text.Json.Serialization;
+using GymManagement.Application;
+using GymManagement.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
