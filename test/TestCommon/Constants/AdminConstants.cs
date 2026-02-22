cd1acd2d0762008fe67f1bd9a9714e65617d4189
Wayfaring Tenno <120097440+Tenno641@users.noreply.github.com>
2026-02-22 05:27:07 +0200

    Feature/test domain (#12)
    * [ADD] Test Folder
    
    * [ADD] Unit Test for adding gym to specific subscription

---
+++ test/TestCommon/Constants/AdminConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TestCommon.Constants;
+
+public static partial class Constants
+{
+    public static class Admin
+    {
+        public readonly static Guid Id = Guid.CreateVersion7();
+    }
+}
