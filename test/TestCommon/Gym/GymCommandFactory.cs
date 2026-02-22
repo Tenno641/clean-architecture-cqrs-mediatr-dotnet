0a4147aa9cb3ed79fce0ddf536f6a4cdfb38c759
Wayfaring Tenno <120097440+Tenno641@users.noreply.github.com>
2026-02-22 10:22:11 +0200

    Feature/application layer tests (#13)
    * [ADD] Behavior Request Handler Test with no validation errors
    
    * [ADD] Validation Behavior test when it's not valid"
    [FIX] Mocking Async method instead"
    
    * [FIX] Adding cancellation token to validate method

---
+++ test/TestCommon/Gym/GymCommandFactory.cs
@@ -0,0 +1,17 @@
+﻿using GymManagement.Application.Gyms.Commands.CreateGym;
+
+namespace TestCommon.Gym;
+
+public static class GymCommandFactory
+{
+    public static CreateGymCommand CreateCreateGymCommand(
+        Guid? subscriptionId = null,
+        string? name = null)
+    {
+        CreateGymCommand command = new CreateGymCommand(
+            SubscriptionId: subscriptionId ?? Constants.Constants.Subscription.Id,
+            Name: name ?? Constants.Constants.Gym.Name);
+
+        return command;
+    }
+}
